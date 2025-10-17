ea80d2fa1321a62e14509d47ba409a6d8264cdde
Kornel Pustelak <92749743+FilozoFja@users.noreply.github.com>
2025-10-17 19:25:59 +0200

    wip-test-project

---
+++ YAPA.Tests/AuthTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace YAPA.Tests
+{
+    public class AuthTests
+    {
+        [Fact]
+        public async Task UserLogin()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+
+            var client = application.CreateClient();
+
+            var result = await client.PostAsJsonAsync("/api/auth/login", new LoginRequest
+            {
+                Email = "admin@admin.com",
+                Password = "Admin123!"
+            });
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
